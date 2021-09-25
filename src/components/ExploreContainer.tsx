c15a9409a2e9d34245158ca4c957b28140100204
Abhishek Sarangi <>
2021-09-25 17:47:28 +0530

    first commit

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,16 @@
+import './ExploreContainer.css';
+import { IonButton } from '@ionic/react';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>Ready to create an app?</strong>
+      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton color="primary">Click Me</IonButton>
+    </div>
+  );
+};
+
+export default ExploreContainer;
